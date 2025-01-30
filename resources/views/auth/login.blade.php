2439c258880a97f7e202607659233ea6c9ebd414
aleksey <av.pavlow@gmail.com>
2025-01-30 09:06:13 +0300

    Смена шаблонов авторизации на более современный

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,9 @@
-@extends('layouts.app')
-
-@section('content')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Вход
+        </h2>
+    </x-slot>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -70,4 +73,4 @@
         </div>
     </div>
 </div>
-@endsection
+</x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,7 +84,7 @@
             </div>
             <div class="row">
                 <div class="col-12">
-                    {{ $slot }}
+                    {{ $slot}}
                 </div>
             </div>
         </div>
